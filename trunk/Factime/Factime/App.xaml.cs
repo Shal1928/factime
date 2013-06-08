99cf38b024637047c57e7a711f784b582a3d1f99
Shal1928 <Shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-08 08:05:35 +0000

    Complete MVVM architecture

---
+++ trunk/Factime/Factime/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Factime.ViewModels;
+using Factime.Views;
+using UseAbilities.MVVM.Managers;
+
+namespace Factime
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var relationsViewToViewModel = new Dictionary<Type, Type>
+                                         {
+                                            {typeof (MainWindowViewModel), typeof (MainWindowView)}
+                                         };
+
+            ViewManager.RegisterViewViewModelRelations(relationsViewToViewModel);
+            ViewModelManager.ActiveViewModels.CollectionChanged += ViewManager.OnViewModelsCoolectionChanged;
+
+            var startupWindow = new MainWindowViewModel();
+            //Loader(StaticHelper.IoCcontainer);
+
+            //var startupWindow = StaticHelper.IoCcontainer.Resolve<MainWindowViewModel>();
+            startupWindow.Show();
+        }
+
+        //private static void Loader(IoC ioc)
+        //{
+        //    ioc.RegisterSingleton<IXmlStore<HitSettings>, HitSettingsStore>();
+        //    //ioc.RegisterSingleton<DBStore<Requests>, RequestsDBStore>();
+        //}
+    }
+}
