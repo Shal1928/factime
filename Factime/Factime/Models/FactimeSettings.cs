672aeb40470f0bf22aac5cb7b18a161fbe6c773b
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-10 04:38:50 +0000

    Add all models;

---
+++ Factime/Factime/Models/FactimeSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Factime.Models
+{
+    [DataContract]
+    public class FactimeSettings
+    {
+        [DataMember]
+        public TimeSpan DefaultStart
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public TimeSpan DefaultEnd
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public TimeSpan ShortEnd
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<DateTime> DefaultHolidaysCollection
+        {
+            get; 
+            set;
+        }
+
+        [DataMember]
+        public List<DateTime> DefaultPreHolidaysCollection
+        {
+            get;
+            set;
+        }
+    }
+}
