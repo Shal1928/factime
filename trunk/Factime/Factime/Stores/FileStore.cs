abd1e87da5115145cb3e4ac2f6d1cf11b3d3f53f
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-10 06:23:25 +0000

    A: 1.0.0.0; F: 1.0.0.3; Add base Stores;

---
+++ trunk/Factime/Factime/Stores/FileStore.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UseAbilities.IoC.Stores;
+
+namespace Factime.Stores
+{
+    public class FileStore : StoreBase<string>, IFileStore<string>
+    {
+        public virtual string FileName
+        {
+            get;
+            set;
+        }
+
+        public override string Load()
+        {
+            var file = new StreamReader(FileName);
+            var fileData = file.ReadLine();
+
+            file.Close();
+            return fileData;
+        }
+    }
+
+    public class FileStore<T> : StoreBase<T>, IFileStore<T>
+    {
+        public virtual string FileName
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ trunk/Factime/Factime/Stores/XMLStore.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UseAbilities.IoC.Stores;
+using UseAbilities.XML.Serialization;
+
+namespace Factime.Stores
+{
+    public class XmlStore<T> : FileStore<T>, IXmlStore<T>
+    {
+        public override void Save(T storeObject)
+        {
+            SerializationUtility.Serialize(storeObject, FileName);
+        }
+
+        public override T Load()
+        {
+            if (File.Exists(FileName)) throw new FileNotFoundException(FileName);
+            return SerializationUtility.Deserialize<T>(FileName);
+        }
+    }
+}
