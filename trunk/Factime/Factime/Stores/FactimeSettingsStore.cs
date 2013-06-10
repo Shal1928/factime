4e7ccd1244c33bfc02fc01e2b40feae89d809730
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-10 06:48:44 +0000

    Add FactimeSettingsStore

---
+++ trunk/Factime/Factime/Stores/FactimeSettingsStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Factime.Models;
+using UseAbilities.XML.Serialization;
+
+namespace Factime.Stores
+{
+    public class FactimeSettingsStore : XmlStore<FactimeSettings>
+    {
+        #region Singleton implementation
+
+        private FactimeSettingsStore()
+        {
+
+        }
+
+        private static readonly FactimeSettingsStore Instance = new FactimeSettingsStore();
+        public static FactimeSettingsStore GetInstance()
+        {
+            return Instance;
+        }
+
+        #endregion
+
+        private const string FILE_NAME = "FactimeSettingsStore.xml";
+        public override string FileName
+        {
+            get
+            {
+                return FILE_NAME;
+            }
+        }
+
+        public override FactimeSettings Load()
+        {
+            if (!File.Exists(FileName)) base.Save(new FactimeSettings
+            {
+                DefaultStart = new TimeSpan(9,0,0),
+                DefaultEnd = new TimeSpan(18, 0, 0),
+                DefaultPreHolidaysCollection = new List<DateTime>(),
+                DefaultHolidaysCollection = new List<DateTime>()
+            });
+
+            return SerializationUtility.Deserialize<FactimeSettings>(FileName);
+        }
+    }
+}
