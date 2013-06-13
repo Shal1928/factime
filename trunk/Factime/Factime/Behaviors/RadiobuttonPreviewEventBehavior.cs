70e23d5777ca01a19d41974a9d39bbbdd6acaf5d
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-13 08:17:37 +0000

    Attempt for create universal Calendar Control for selection days type

---
+++ trunk/Factime/Factime/Behaviors/RadiobuttonPreviewEventBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Factime.Behaviors
+{
+    public class RadioButtonPreviewEventBehavior : Behavior<RadioButton>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseLeftButtonDown += (sender, e) =>
+            {
+                AssociatedObject.IsChecked = !AssociatedObject.IsChecked;
+            };
+        }
+    }
+}
