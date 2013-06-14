4f4803f1ecf74d206f8f0e06fca787c316c447e9
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-14 10:30:21 +0000

    50/50

---
+++ Factime/Factime/Models/WeekWrapper.cs
@@ -0,0 +1,47 @@
+﻿namespace Factime.Models
+{
+    public class WeekWrapper
+    {
+        public CalendarDay Monday
+        {
+            get; 
+            set;
+        }
+
+        public CalendarDay Tuesday
+        {
+            get;
+            set;
+        }
+
+        public CalendarDay Wednesday
+        {
+            get;
+            set;
+        }
+
+        public CalendarDay Thursday
+        {
+            get;
+            set;
+        }
+
+        public CalendarDay Friday
+        {
+            get;
+            set;
+        }
+
+        public CalendarDay Saturday
+        {
+            get;
+            set;
+        }
+
+        public CalendarDay Sunday
+        {
+            get;
+            set;
+        }
+    }
+}
