71d2489cbecba4aa70e9d3e20938bb46e9ba9718
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-17 08:38:10 +0000

    Add TestConsoleApplication for test

---
+++ Factime/TestConsoleApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UseAbilities.Extensions.EnumerableExt;
+
+namespace TestConsoleApplication
+{
+    public class IntWrap
+    {
+        public IntWrap(int i)
+        {
+            Value = i;
+        }
+
+        public int Value
+        {
+            get; 
+            set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 1, 1, 1, 2, 3, 2, 2, 3, 3, 4, 4, 5, 5, 6, 3 ,6, 7 };
+
+            var nums2 = new[]
+                            {
+                                new IntWrap(8),
+                                new IntWrap(4),
+                                new IntWrap(2),
+                                new IntWrap(2),
+                                new IntWrap(4),
+                                new IntWrap(7),
+                                new IntWrap(4),
+                            };
+
+            IEnumerable<int> top5 = nums
+                       .GroupBy(i => i)
+                       .OrderByDescending(g => g.Count())
+                       .Take(5)
+                       .Select(g => g.Key);
+
+            //Console.WriteLine(nums2.GetFrequentlyValue().Value);
+            
+            Console.ReadLine();
+        }
+    }
+}
