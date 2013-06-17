914a28c30ebad82422e6773ebea15cc74de530e3
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-17 07:40:41 +0000

    Resolve trouble for DataGridTempalteColumn Binding; Add DataGridTemplateColumn inhereted from DataGridBoundColumn;

--- Factime/Factime/Controls/DataGridTemplateColumnExt.cs
+++ Factime/Factime/Controls/DataGridTemplateColumnExt.cs
@@ -1,16 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using Factime.Models;
 
 namespace Factime.Controls
 {
-    public class DataGridTemplateColumnExt : DataGridTemplateColumn
+    public class DataGridTemplateColumnExt : DataGridBoundColumn
     {
-        //public DataGridTemplateColumnExt()
+        //#region DependencyProperty DataContext
+
+        //public static readonly DependencyProperty DataContextProperty =
+        //    DependencyProperty.Register("DataContext",
+        //                                typeof(object),
+        //                                typeof(DataGridTemplateColumnExt),
+        //                                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits, OnDataContextChanged)
+        //                                );
+
+        //public object DataContext
         //{
-        //    base.DisplayIndex
+        //    get
+        //    {
+        //        return (object)GetValue(DataContextProperty);
+        //    }
+        //    set
+        //    {
+        //        SetValue(DataContextProperty, value);
+        //    }
         //}
+
+        //private static void OnDataContextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    //
+        //}
+
+        //#endregion
+
+        public DataTemplate CellTemplate
+        {
+            get; 
+            set;
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            if (Binding == null) return null;
+            var binding = new Binding(((Binding) Binding).Path.Path)
+                              {
+                                  Source = dataItem
+                              };
+
+            var content = new ContentControl
+                              {
+                                  ContentTemplate = CellTemplate
+                              };
+            content.SetBinding(ContentControl.ContentProperty, binding);
+
+            return content;
+        }
+
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            return GenerateElement(cell, dataItem);
+        }
     }
 }
