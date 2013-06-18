956d290addbea4b26efaf7ef601a3164357419ad
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-18 03:44:42 +0000

    Bound RadioButtons to ViewModel Data

---
+++ trunk/Factime/Factime/Converters/DayTypeToBoolConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Factime.Models;
+using UseAbilities.WPF.Converters.Base;
+
+namespace Factime.Converters
+{
+    public class DayTypeToBoolConverter : ConverterBase<DayTypeToBoolConverter>
+    {
+        public DayTypeToBoolConverter()
+        {
+            //
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DayType) && !(parameter is DayType)) throw new Exception(" Value or parametr is not DayType!");
+
+            var displayType = (DayType)value;
+            var parametrValue = (DayType)parameter;
+
+            return displayType == parametrValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool) && !(parameter is DayType)) throw new Exception(" Value is not bool or parametr is not DayType!");
+
+            if((bool)value) return (DayType)parameter;
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
