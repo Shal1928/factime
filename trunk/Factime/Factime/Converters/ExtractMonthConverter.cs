d15e531cd2a9a0f8c220a56a1757a58dc3dc0ca7
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-18 01:48:31 +0000

    F: 1.0.2.0; Resolve days marking

---
+++ trunk/Factime/Factime/Converters/ExtractMonthConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using UseAbilities.WPF.Converters.Base;
+
+namespace Factime.Converters
+{
+    public class ExtractMonthConverter : ConverterBase<ExtractMonthConverter>
+    {
+        public ExtractMonthConverter()
+        {
+            //
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTime)) throw new ArgumentException("Value is not DateTime into ExtractMonthConverter");
+            return ((DateTime) value).Month;
+        }
+    }
+}
