9a7f97991122d611fa0e03b793c34d0272bae9a1
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-19 10:13:41 +0000

    Update Visual Styles

---
+++ trunk/Factime/Factime/Converters/StateHolidayMultiConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using UseAbilities.WPF.Converters.Base;
+
+namespace Factime.Converters
+{
+    public class StateHolidayMultiConverter : MultiConverterBase<StateHolidayMultiConverter>
+    {
+        public StateHolidayMultiConverter()
+        {
+            //
+        }
+
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!(values[0] is bool) && !(values[1] is bool)) throw new Exception("Values is not bool!");
+            if((bool) values[0]) return true;
+
+            return (bool) values[1];
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new[] { false, value };
+        }
+    }
+}
