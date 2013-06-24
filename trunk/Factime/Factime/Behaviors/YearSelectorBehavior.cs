f3880513a95f264ef0958cd552755f34b3f94db8
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-06-24 08:24:53 +0000

    Add YearSelector and connect him to application;

---
+++ trunk/Factime/Factime/Behaviors/YearSelectorBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+using System.Windows.Shapes;
+
+namespace Factime.Behaviors
+{
+    public class YearSelectorBehavior : Behavior<Path>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseLeftButtonDown += (sender, e) =>
+            {
+                if (IsIncrease) SelectedYear++;
+                else SelectedYear--;
+            };
+        }
+
+        #region SelectedYear
+
+        public static readonly DependencyProperty SelectedYearProperty =
+            DependencyProperty.Register("SelectedYear",
+                                        typeof(int),
+                                        typeof(YearSelectorBehavior),
+                                        new UIPropertyMetadata(0, OnSelectedYearPropertyChanged)
+                                        );
+
+        public int SelectedYear
+        {
+            get
+            {
+                return (int)GetValue(SelectedYearProperty);
+            }
+            set
+            {
+                SetValue(SelectedYearProperty, value);
+            }
+        }
+
+        private static void OnSelectedYearPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //
+        }
+
+        #endregion
+
+        #region IsIncrease
+
+        public static readonly DependencyProperty IsIncreaseProperty =
+            DependencyProperty.Register("IsIncrease",
+                                        typeof(bool),
+                                        typeof(YearSelectorBehavior),
+                                        new UIPropertyMetadata(false, OnIsIncreaseChanged)
+                                        );
+
+        public bool IsIncrease
+        {
+            get
+            {
+                return (bool)GetValue(IsIncreaseProperty);
+            }
+            set
+            {
+                SetValue(IsIncreaseProperty, value);
+            }
+        }
+
+        private static void OnIsIncreaseChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //
+        }
+
+        #endregion
+    }
+}
