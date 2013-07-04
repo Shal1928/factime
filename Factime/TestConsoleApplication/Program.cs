1eef3397d8e2dabf35fe1f002092e604d533e7e5
shal1928 <shal1928@89867302-14e0-dd21-f6d9-f25cd1bc0d2b>
2013-07-04 08:53:45 +0000

    Emit Attempts for create own proxy with blackjack and hookers!

--- Factime/TestConsoleApplication/Program.cs
+++ Factime/TestConsoleApplication/Program.cs
@@ -1,73 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using UseAbilities.Extensions.EnumerableExt;
 
 namespace TestConsoleApplication
 {
-    public class IntWrap
+    public interface I
     {
-        public IntWrap(int i)
+        void M();
+    }
+
+    public class A
+    {
+        public virtual void M()
         {
-            Value = i;
+            Console.WriteLine("In method A.M");
         }
 
-        public int Value
+        private string _stringProperty;
+        public virtual string StringProperty
         {
-            get; 
-            set;
+            get
+            {
+                return _stringProperty;
+            }
+            set
+            {
+                Console.WriteLine("Important action");
+                _stringProperty = value;
+            }
         }
     }
 
+    //// The object of this code example is to emit code equivalent to 
+    //// the following C# code: 
+    //// 
+    //public class C : A, I
+    //{
+    //    public override void M()
+    //    {
+    //        Console.WriteLine("Overriding A.M from C.M");
+    //    }
+
+    //    // In order to provide a different implementation from C.M when  
+    //    // emitting the following explicit interface implementation,  
+    //    // it is necessary to use a MethodImpl. 
+    //    // 
+    //    void I.M()
+    //    {
+    //        Console.WriteLine("The I.M implementation of C");
+    //    }
+    //}
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var nums = new[] { 1, 1, 1, 2, 3, 2, 2, 3, 3, 4, 4, 5, 5, 6, 3 ,6, 7 };
+            var name = "DefineMethodOverrideExample";
+            AssemblyName asmName = new AssemblyName(name);
+            AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder mb = ab.DefineDynamicModule(name, name + ".dll");
 
-            var nums2 = new[]
-                            {
-                                new IntWrap(8),
-                                new IntWrap(4),
-                                new IntWrap(2),
-                                new IntWrap(2),
-                                new IntWrap(4),
-                                new IntWrap(7),
-                                new IntWrap(4),
-                            };
+            TypeBuilder tb = mb.DefineType("C", TypeAttributes.Public, typeof(A));
+            tb.AddInterfaceImplementation(typeof(I));
 
-            IEnumerable<int> top5 = nums
-                       .GroupBy(i => i)
-                       .OrderByDescending(g => g.Count())
-                       .Take(5)
-                       .Select(g => g.Key);
+            // Build the method body for the explicit interface  
+            // implementation. The name used for the method body  
+            // can be anything. Here, it is the name of the method, 
+            // qualified by the interface name. 
+            //
+            MethodBuilder mbIM = tb.DefineMethod("I.M", MethodAttributes.Private | 
+                                                        MethodAttributes.HideBySig |
+                                                        MethodAttributes.NewSlot | 
+                                                        MethodAttributes.Virtual |
+                                                        MethodAttributes.Final, null, Type.EmptyTypes);
 
-            //8 4 2 7
-            IEnumerable<IntWrap> listNums2 = new List<IntWrap>
-                                {
-                                    new IntWrap(8),
-                                    new IntWrap(4),
-                                    new IntWrap(2),
-                                    new IntWrap(2),
-                                    new IntWrap(4),
-                                    new IntWrap(7),
-                                    new IntWrap(4),
-                                };
-            var distinctCollection = listNums2.GroupBy(x => x.Value).Select(y => y.First());
+            ILGenerator il = mbIM.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "The I.M implementation of C"); //Pushes a new object reference to a string literal stored in the metadata.
+            il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine",new[] { typeof(string) }));
+            il.Emit(OpCodes.Ret);
+
+            // DefineMethodOverride is used to associate the method  
+            // body with the interface method that is being implemented. 
+            //
+
+            tb.DefineMethodOverride(mbIM, typeof(I).GetMethod("M"));
+
+            MethodBuilder mbM = tb.DefineMethod("M", MethodAttributes.Public | 
+                                                     MethodAttributes.ReuseSlot |
+                                                     MethodAttributes.Virtual | 
+                                                     MethodAttributes.HideBySig, null, Type.EmptyTypes);
+            il = mbM.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "Overriding A.M from C.M");
+            il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
+            il.Emit(OpCodes.Ret);
 
 
-            //foreach (var intWrap in distinctCollection)
-            //{
-            //    Console.WriteLine(intWrap.Value);
-            //}
 
-            var stringValue = "1245";
-            var timeSpanValue = new TimeSpan(12, 46, 00);
-            var time = TimeSpan.Parse(stringValue);
 
-            Console.WriteLine("From string: {0}", time);
-            Console.WriteLine("To string: {0}", timeSpanValue.ToString().Remove(3));
+            //My attempt
+            //***
+            //***
+            //***
+
+            var targetProperty = typeof(A).GetProperty("StringProperty");
+            const MethodAttributes getSetAttributes = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig | MethodAttributes.Virtual;
+            FieldBuilder valueField = tb.DefineField("_" + targetProperty.Name, targetProperty.PropertyType, FieldAttributes.Private);
+            MethodBuilder valuePropertyGet = tb.DefineMethod("get_StringProperty", getSetAttributes, null, new[] { targetProperty.PropertyType });
+
+            il = valuePropertyGet.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, valueField); //load from field
+            il.Emit(OpCodes.Ret);
+
+            MethodBuilder valuePropertySet = tb.DefineMethod("set_StringProperty", getSetAttributes, null, new[] { targetProperty.PropertyType });
+
+            il = valuePropertySet.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Ldstr, "Overridning StringProperty");
+            il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
+            il.Emit(OpCodes.Call, typeof(A).GetProperty("StringProperty").GetSetMethod());
+            //il.Emit(OpCodes.Stfld, valueField); //set to field
+            il.Emit(OpCodes.Ret);
+
+            //***
+            //***
+            //***
+
+
+            Type tc = tb.CreateType();
+
+            // Save the emitted assembly, to examine with Ildasm.exe.
+            ab.Save(name + ".dll");
+
+            A test = (A) Activator.CreateInstance(tc);
+
+            MethodInfo mi = typeof(I).GetMethod("M");
+            mi.Invoke(test, null);
+
+            mi = typeof(A).GetMethod("M");
+            mi.Invoke(test, null);
+
+
+            //var targetProperty2 = test.GetType().GetProperty("StringProperty").GetSetMethod();
+            //targetProperty2.Invoke(test, new[] {"1"});
+
+            test.StringProperty = "1";
 
             Console.ReadLine();
         }
